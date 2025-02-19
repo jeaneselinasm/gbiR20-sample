f482e90b0c65339799f6e1efe1e1593ce8298ee8
jeaneselinasm <jeanessmanuhutu@gmail.com>
2025-02-19 13:55:10 +0900

    [update ui] page rayon each

--- app/sub-rayon/[rayon]/page.tsx
+++ app/sub-rayon/[rayon]/page.tsx
@@ -1,18 +1,22 @@
-export async function generateStaticParams() {
-  const rayons = ['a', 'b', 'c', 'd']; // Expand this up to 11 later
+import { subRayonData } from "@/lib/data";
 
-  return rayons.map((rayon) => ({ rayon }));
+export async function generateStaticParams() {
+  return subRayonData.map((rayon) => ({ rayon: rayon.id }));
 }
 
-export default function RayonPage({ params}) {
-  const { rayon } = params; // Next.js automatically passes params
+export default function RayonPage({ params }) {
+  const { rayon } = params;
+  const rayonDetails = subRayonData.find((r) => r.id === rayon);
+
+  if (!rayonDetails) {
+    return <h1>Rayon Not Found</h1>;
+  }
 
   return (
     <div className="container mx-auto px-4 py-8 bg-purple-50">
-      <h1 className="text-4xl font-bold mb-6 ">Sub Rayon {rayon.toUpperCase()}</h1>
-
-       
-        
+      <h1 className="text-4xl font-bold mb-6">Sub Rayon {rayonDetails.name}</h1>
+      <p className="text-lg text-gray-700">Location: {rayonDetails.location}</p>
+      <p className="text-gray-600 text-justify">{rayonDetails.description}</p>
     </div>
   );
 }

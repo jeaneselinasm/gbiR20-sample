048999751585cfaa6985adfd5e9671371195cb5e
jeaneselinasm <jeanessmanuhutu@gmail.com>
2025-07-20 11:46:19 +0900

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,9 +6,9 @@ export default function Footer() {
       <div className="container mx-auto px-4 py-8">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           <div>
-            <h3 className="text-lg font-semibold mb-4">GBI My Home Rawasari</h3>
+            <h3 className="text-lg font-semibold mb-4">📍 Grace Fellowship Church</h3>
             <p className="text-gray-600">
-              House of Mission and Evangelism
+              Pc Street 12 East Area
             </p>
           </div>
           <div>
@@ -38,46 +38,42 @@ export default function Footer() {
             <ul className="space-y-2 text-gray-600 ">
               <li className="">
                 <Link
-                  href="https://www.instagram.com/gbimyhome/"
+                  href="https://www.instagram.com/"
                   className="flex items-center hover:text-gray-900"
                 >
-                <Instagram className="h-6 w-6 mr-2 " /> <span>@gbimyhome</span>
+                <Instagram className="h-6 w-6 mr-2 " /> <span>@Grace Fellowship Church</span>
                 </Link>
               </li>
               <li className="">
                 <Link
-                  href="https://www.facebook.com/profile.php?id=100082063553461"
+                  href="https://www.facebook.com/"
                   className="flex items-center hover:text-gray-900"
                 >
 
-                <Facebook className="h-6 w-6 mr-2 " /> <span>GBI My Home Rawasari</span>
+                <Facebook className="h-6 w-6 mr-2" /> <span>Grace Fellowship Church</span>
                 </Link>
               </li>
               <li className="">
                 <Link
-                  href="https://www.youtube.com/c/GBIMYHOME/videos"
+                  href="https://www.youtube.com/"
                   className="flex items-center hover:text-gray-900"
                 >
-                <Youtube className="h-6 w-6 mr-2 " /> <span>GBI MY HOME OFFICIAL</span> 
+                <Youtube className="h-6 w-6 mr-2 " /> <span>Grace Fellowship Church Official</span> 
                 </Link>
               </li>
             </ul>
           </div>
           <div>
-            <h3 className="text-lg font-semibold mb-4">Hubungi Kami</h3>
+            <h3 className="text-lg font-semibold mb-4">Contact US</h3>
+            <p className="text-gray-600">📞 (021) 1234-5678</p>
             <p className="text-gray-600">
-              Jalan Rawasari Komplek PSMTI No. 1, RT/RW 001/007, Kampung Bulang,
-              Tanjung Pinang Timur, Kepulauan Riau - 29122
-            </p>
-            <p className="text-gray-600">Phone: (123) 456-7890</p>
-            <p className="text-gray-600">
-              Email: gbimyhomerawasari@gmail.com / tanjungpinangr20@gmail.com
+             📧 hello@gracechurch.id
             </p>
           </div>
         </div>
         <div className="mt-8 border-t border-gray-200 pt-6 text-center">
           <p className="text-red-600">
-            &copy; {new Date().getFullYear()} BAHTRAKU. All rights reserved.
+            &copy; {new Date().getFullYear()} CREATED BY JEANE SELINA | All rights reserved.
           </p>
           <p className="text-red-600">
             This is a demo version for preview purposes only

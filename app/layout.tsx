e093304a24242d7e16c84004e7b879cc4ba1f6c7
jeaneselinasm <jeanessmanuhutu@gmail.com>
2025-07-21 10:25:59 +0900

    update layout body bg

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        <main className="min-h-screen">{children}</main>
+        <main className="min-h-screen bg-violet-50">{children}</main>
         <Footer />
       </body>
     </html>
